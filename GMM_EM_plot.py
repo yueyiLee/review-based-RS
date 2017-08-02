734b9e39199b50ab0d79d6e540c649e24563a321
Eric Lee <jllisiwei1993@163.com>
2017-08-02 22:04:57 +0800

    modify the dataset and plotting

--- GMM_EM_plot.py
+++ GMM_EM_plot.py
@@ -20,9 +20,6 @@ def plot_results(X, Y, means, covariances, index, title):
         if not np.any(Y == i):
             continue
         plt.scatter(X[Y == i, 0], X[Y == i, 1], label=label, color=color, s=20, marker="o")
-        # plt.xlabel('Figure1',fontsize = 'large')
-        # plt.ylabel('Figure2',fontsize = 'large')
-        plt.legend()
 
         # Plot an ellipse to show the Gaussian component
         angle = np.arctan(u[1] / u[0])
@@ -43,21 +40,19 @@ def plot_results(X, Y, means, covariances, index, title):
 # Parameters
 n_samples = 1000
 
-# Generate random sample following a sine curve
+# Generate random sample from two Gaussian Distributions respectively
 np.random.seed(0)
 X = np.zeros((n_samples, 2))
-step = 4. * np.pi / n_samples
 
 for i in range(X.shape[0]):
-    x = i * step - 6.
-    X[i, 0] = x + np.random.normal(0, 0.1)
-    X[i, 1] = 3. * (np.sin(x) + np.random.normal(0, .2))
+    X[i, 0] = np.random.normal(1, 1.5)
+    X[i, 1] = np.random.normal(-0.5, 2)
 
 plt.figure(figsize=(10, 10))
 plt.subplots_adjust(bottom=.04, top=0.95, hspace=.2, wspace=.05,left=.03, right=.97)
 
-# Fit a Gaussian mixture with EM using five components
-gmm = mixture.GaussianMixture(n_components=6, covariance_type='full',max_iter=500).fit(X)
+# Fit a Gaussian mixture with EM using n components
+gmm = mixture.GaussianMixture(n_components=3, covariance_type='full',max_iter=500).fit(X)
 plot_results(X, gmm.predict(X), gmm.means_, gmm.covariances_, 0,'Expectation-maximization')
 
 plt.show()
